f4ebaeacda9432aa908bdae8144d1ef72fc8fb38
Alex Kuhn <kuhn.alexey@gmail.com>
2023-02-05 15:22:08 +0300

    add accuweather api client, dotenv gem

---
+++ app/services/accuweather_client.rb
@@ -0,0 +1,31 @@
+require 'net/http'
+require 'uri'
+
+class AccuweatherClient
+  def get_historical_data(
+    location: ENV['ACCUWEATHER_LOCATION_KEY'],
+    version: 'v1'
+  )
+    begin
+      uri =
+        URI(
+          "#{ENV['ACCUWEATHER_API_URL']}/#{version}" +
+            "/#{location}/historical/24"
+        )
+      uri.query = URI.encode_www_form({ apikey: ENV['ACCUWEATHER_API_KEY'] })
+      http = Net::HTTP.new(uri.host, uri.port)
+      request = Net::HTTP::Get.new(uri)
+
+      response = http.request(request)
+      response.kind_of?(Net::HTTPSuccess) ? JSON.parse(response.body) : nil
+    rescue Timeout::Error,
+           Errno::EINVAL,
+           Errno::ECONNRESET,
+           EOFError,
+           Net::HTTPBadResponse,
+           Net::HTTPHeaderSyntaxError,
+           Net::ProtocolError => e
+      Logger.new("#{Rails.root}/log/accuweather_error.log").error e.message
+    end
+  end
+end
